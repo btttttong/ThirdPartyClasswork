299eae36dc12e095b95daededb9337aade07d836
Supak Rodklang <tong.7147@gmail.com>
2023-10-01 15:36:17 +0700

    commit classwork

--- Task2.py
+++ Task2.py
@@ -7,34 +7,38 @@ def rockyou(user_choice):
     user_win = ''
     com_choice = random.choice(list_choice)
     print('com choice = ', com_choice)
-    if user_choice == com_choice:
-        return 'equal'
-    if user_choice == 'rock':
-        if com_choice == 'scissors':
-            score[0][1] += 1
-            return True
-        else:
-            score[1][1] += 1
-            return False
-    if user_choice == 'paper':
-        if com_choice == 'rock':
-            score[0][1] += 1
-            return True
-        else:
-            score[1][1] += 1
-            return False
-    if user_choice == 'scissors':
-        if com_choice == 'paper':
-            score[0][1] += 1
-            return True
-        else:
-            score[1][1] += 1
-            return False
-
+    if user_choice not in list_choice:
+        return "invalid choice"
+    else:
+        if user_choice == com_choice:
+            return 'Draw'
+        if user_choice == 'rock':
+            if com_choice == 'scissors':
+                score[0][1] += 1
+                return True
+            else:
+                score[1][1] += 1
+                return False
+        if user_choice == 'paper':
+            if com_choice == 'rock':
+                score[0][1] += 1
+                return True
+            else:
+                score[1][1] += 1
+                return False
+        if user_choice == 'scissors':
+            if com_choice == 'paper':
+                score[0][1] += 1
+                return True
+            else:
+                score[1][1] += 1
+                return False
 
 while score[0][1] < 3:
     res = rockyou(input('rock paper or scissors? : '))
-    res = "You!!!" if res == True else "PC" if res == False else 'equal'
+    res = "You!!!" if res == True else "Computer" if res == False else res
     print('who is the winner? : ', res)
-print('static: ', score)
+    print('------------------------------------')
+
+print('static:' + ' VS '.join(str(i) for i in score))
 
